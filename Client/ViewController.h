102846abc1c1bc3a71327bef64cc725e91cab2cd
kleinsche <744197047@qq.com>
2017-08-20 14:19:32 +0800

    Initial Commit

---
+++ Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Client
+//
+//  Created by Kleinsche on 2017/8/20.
+//  Copyright © 2017年 Kleinsche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Client/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Client
+//
+//  Created by Kleinsche on 2017/8/20.
+//  Copyright © 2017年 Kleinsche. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
